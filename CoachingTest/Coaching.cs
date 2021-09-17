af914ae73b6836903fa98570d27d52fd09993337
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 18:23:40 +0400

    Add files via upload

---
+++ CoachingTest/Coaching.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoachingTest
+{
+    class Coaching
+    {
+        //1. IV - private var - associated with Implemented Properties
+        private int playersCount;
+
+        //2. Auto-Implemented Properties - Do Not require VALIDATION
+        public string CoachingType { get; set; }
+        public string ClassTiming { get; set; }
+        public double Charges { get; set; }
+
+        //3. Implemented Properties - Require Validation
+        public int PlayersCount
+        {
+            get
+            {
+                return playersCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    playersCount = value;
+                }
+            }
+        }
+        
+
+        //4. Parameterised Constructor - publicCoaching (Properties)
+        public Coaching(string type, string timing, double charges, int players)
+        {
+            CoachingType = type;
+            ClassTiming = timing;
+            PlayersCount = players;
+            Charges = charges;
+        }
+
+
+        //5. Methods
+        public void Details()
+        {
+            Console.WriteLine($"The charges are: {Charges:C} \n" +
+                $"The number of players is: {PlayersCount}");
+        }
+
+
+    }
+}
---
+++ CoachingTest/CoachingTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CoachingTest
+{
+    class CoachingTest
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 4.12: The Coaching class
+
+            //Create a class called Coaching that includes four (4) pieces of information
+            ////as auto-implemented properties - the coaching type (sport) (type string), 
+            //the number of players (type int), class timings (type string) and charges (type double). 
+            //Your class should have a constructor that initializes the four automatic properties 
+            //and assumes that the values are provided.
+            //Provide a method DisplayDetails that displays all the information.
+            //The charges should be represented as $.
+            //Write a test app named CoachingTest that demonstrates the class.
+
+
+            //Create the objeect
+            Coaching coaching1 = new Coaching("Tennis", "Morning", 10.0, 4);
+
+            //Accessing Object Properties
+            Console.WriteLine($"The Coaching Type is: {coaching1.CoachingType}");
+            Console.WriteLine($"The Class time is: {coaching1.ClassTiming}");
+
+            //Accessing the methods
+            coaching1.Details();
+            Console.ReadLine();
+            
+        }
+    }
+}
